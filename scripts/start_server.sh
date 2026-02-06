5c5e3cad4a5846f2bc4d75b457009ca549f3e807
Delaney Rasband <delaney.rasband@gmail.com>
2026-02-06 15:42:12 -0700

    Enhance start_server.sh with error handling options
    Added set -eux to script for better error handling.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-cd /home/ec2-user/paradise-donut-app
-nohup node server.js > app.log 2>&1 &
+set -eux
 
+cd /home/ec2-user/paradise-donut-app
+
+# Start Node app in the background and redirect logs
+nohup node server.js > app.log 2>&1 &
